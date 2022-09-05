452d90711c1c921f1fa71da139db3c37a32d79ab
HariprakashM <hariprakashm20@gmail.com>
2022-09-05 10:18:04 +0530

    done

---
+++ connection.js
@@ -0,0 +1,18 @@
+const dotenv=require('dotenv').config();
+const mongoose=require('mongoose');
+// var mongoURL='mongodb+srv://Hariprakash:Hariprakash20@cluster0.3wyvyo5.mongodb.net/?retryWrites=true&w=majority'
+var mongoURL=process.env.URL;
+
+mongoose.connect(mongoURL,{useUnifiedTopology:true , useNewUrlParser:true});
+
+var connection=mongoose.connection;
+
+connection.on('error' , ()=>{
+    console.log('Mongo DB connection failed')
+})
+
+connection.on('connected' , ()=>{
+    console.log('Mongo DB connection successful')
+})
+
+module.exports=mongoose
