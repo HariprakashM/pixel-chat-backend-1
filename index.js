18cdce663f68bb8f8376d1c73ca393c4c76f7e4a
HariprakashM <hariprakashm20@gmail.com>
2023-01-03 09:17:32 +0530

    hp

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 const PORT=process.env.PORT || 5000;
 const io = require('socket.io')(server, {
     cors: {
